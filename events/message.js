1bb204ea306d342742a682b768b607a1d5583b8c
smixqse <18102488+smixqse@users.noreply.github.com>
2018-05-22 13:15:40 -0300

    Eventos agora são em arquivos separados.

---
+++ events/message.js
@@ -0,0 +1,15 @@
+var cooldown = new Set();
+module.exports = (Discord, bot, message) => {
+    if (message.author.bot) return;
+    if (message.guild.id != bot.config.guild) return;
+    if (!message.content.split(" ")[0].startsWith(bot.config.prefix)) return;
+    let command = message.content.split(" ")[0].slice(bot.config.prefix.length);
+    let args = message.content.split(" ").slice(1);
+    const cmd = bot.commands.get(command);
+    if (!cmd) return;
+    if (cooldown.has(message.author.id)) message.channel.send("<@" + message.author.id + "> Aguarde 3 segundos pra usar comandos novamente.");
+    if (cooldown.has(message.author.id)) return;
+    cooldown.add(message.author.id);
+    setTimeout(() => { cooldown.delete(message.author.id); }, 3000);
+    cmd.run(Discord, bot, message, args);
+}
--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Recebido");
-  response.sendStatus(200);
+    console.log(Date.now() + " Ping Recebido");
+    response.sendStatus(200);
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
 fs.readdir("./commands/", (err, files) => {
@@ -28,25 +28,23 @@ fs.readdir("./commands/", (err, files) => {
         if (!file.endsWith(".js")) return;
         let props = require(`./commands/${file}`);
         let commandNames = props.aliases;
-        commandNames.forEach(commandName => {bot.commands.set(commandName, props);});
+        commandNames.forEach(commandName => { bot.commands.set(commandName, props); });
     });
     console.log("[Info] Comandos carregados!");
 })
 
-bot.on("message", message => {
-    if (message.author.bot) return;
-    if (message.guild.id != bot.config.guild) return;
-    if (!message.content.split(" ")[0].startsWith(bot.config.prefix)) return;
-    let command = message.content.split(" ")[0].slice(bot.config.prefix.length);
-    let args = message.content.split(" ").slice(1);
-    const cmd = bot.commands.get(command);
-    if (!cmd) return;
-    if (cooldown.has(message.author.id)) message.channel.send("<@" + message.author.id + "> Aguarde 3 segundos pra usar comandos novamente.");
-    if (cooldown.has(message.author.id)) return;
-    cooldown.add(message.author.id);
-    setTimeout(() => {cooldown.delete(message.author.id);}, 3000);
-    cmd.run(Discord, bot, message, args);
-})
+fs.readdir("./events/", (err, files) => {
+    console.log("[Info] Carregando eventos...");
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        bot.on(eventName, event.bind(null, Discord, bot));
+        delete require.cache[require.resolve(`./events/${file}`)];
+    })
+    console.log("[Info] Eventos carregados!");
+});
 
-bot.login(process.env.TOKEN).then(() => {console.log("[Info] Logado com sucesso!");}).catch(() => {console.log("[Info] O bot não pôde logar."); process.exit();});
+bot.login(process.env.TOKEN).then(() => { console.log("[Info] Logado com sucesso!"); }).catch(() => { console.log("[Info] O bot não pôde logar."); process.exit(); });
 console.log("[Info] Logando...")

6f737f87f793feefde146fcec4bab81ddf38fba0
BlueSlimee <33792776+BlueSlimee@users.noreply.github.com>
2018-05-22 12:33:02 -0400

    🐌 | Update Slowmode <1/2>

--- events/message.js
+++ events/message.js
@@ -1,6 +1,17 @@
+var Enmap = require("enmap");
 var cooldown = new Set();
+var slowmode = new Enmap();
 module.exports = (Discord, bot, message) => {
     if (message.author.bot) return;
+    if (slowmode.has(message.channel.id)) {
+       var user = message.member;
+       var role = bot.roles.find("448291438887698432");
+       var time = slowmode.get(message.channel.id);
+       user.addRole(role);
+       setTimeout(() => {
+           user.removeRole(role);
+       }, time);
+    }
     if (message.guild.id != bot.config.guild) return;
     if (!message.content.split(" ")[0].startsWith(bot.config.prefix)) return;
     let command = message.content.split(" ")[0].slice(bot.config.prefix.length);
@@ -11,5 +22,5 @@ module.exports = (Discord, bot, message) => {
     if (cooldown.has(message.author.id)) return;
     cooldown.add(message.author.id);
     setTimeout(() => { cooldown.delete(message.author.id); }, 3000);
-    cmd.run(Discord, bot, message, args);
+    cmd.run(Discord, bot, message, args, slowmode);
 }
